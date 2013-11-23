5c9d394bb4e66c65c5221eb72db6de2fa0eb4676
Jacqueline Kay <jkay1@cs.swarthmore.edu>
2013-11-23 13:23:47 -0500

    Actually fixed yield.sh

--- Testing/MicroTests/yield.sh
+++ Testing/MicroTests/yield.sh
@@ -7,17 +7,9 @@ numruns=20
 yieldnum=0
 noyieldnum=0
 
-for j in {1..$numruns}; do
-    $yieldnum=`./simple_test $n`
-    #`expr $yieldnum + $out`
-#done
-
-#echo $yieldnum
-#for j in {1..$numruns}; do
-    $noyieldnum=`./simple_test_noyield $n`
-    #=`expr $noyieldnum + $out`
-#done
-#echo $noyieldnum
-
+for j in {1..100}
+do
+    yieldnum=`./simple_test $n`
+    noyieldnum=`./simple_test_noyield $n`
     echo `python -c "print float($yieldnum - $noyieldnum)/(2*$n)"`
 done
