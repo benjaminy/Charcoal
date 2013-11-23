d3d08eca9030bfbd72a26aa48fd5b38c7642ca47
Joshua Gluck <jgluck2@cs.swarthmore.edu>
2013-11-23 11:52:55 -0500

    Copied over unyield tests from master.

---
+++ Testing/MicroTests/simple_unyield_test.c
@@ -0,0 +1,41 @@
+#include <charcoal_runtime.h>
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+
+
+void fa( void *a )
+{
+	int n = ((int*) a)[0];
+	int i = 0;
+	double j = 0;
+	for(i = 0; i < n; i++){
+		j+=i;
+		OPA_store_int(&(__charcoal_activity_self()->container->timeout), 1);
+	#if YIELDING==1
+		__charcoal_yield();
+	#endif
+	}
+	//printf("%f", j);
+}
+
+int __charcoal_replace_main( int argc, char **argv )
+{
+	int n = 1000000;
+	if (argc > 1){
+		n = atoi(argv[1]);
+	}
+    //__charcoal_activity_self()->container->max_time = 0;
+	struct timeval starttime;
+	struct timeval endtime;
+        OPA_store_int(&(__charcoal_activity_self()->container->unyielding), 1);
+	gettimeofday(&starttime, NULL);
+    __charcoal_activity_t* a = __charcoal_activate(fa, (void*) &n );
+    __charcoal_activity_t* b = __charcoal_activate(fa, (void*) &n );
+
+    __charcoal_activity_join( a );
+    __charcoal_activity_join( b );
+	gettimeofday(&endtime, NULL);
+	double diff = (endtime.tv_sec)*1000000+endtime.tv_usec - (starttime.tv_sec)*1000000+starttime.tv_usec;
+	printf("%d\n", (int) diff); //microseconds
+}
---
+++ Testing/MicroTests/unyield.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+make clean
+make simple_unyield_test
+make simple_unyield_test_noyield
+n=1000000
+numruns=1000000000
+yield_total=0
+yieldnum=0
+noyieldnum=0
+for j in {1..$numruns}; do
+		out=`./simple_unyield_test $n`
+		yieldnum=`expr $yieldnum + $out`
+done
+
+echo $yieldnum
+for j in {1..$numruns}; do
+		out=`./simple_unyield_test_noyield $n`
+		noyieldnum=`expr $noyieldnum + $out`
+done
+echo $noyieldnum
+#Deleted +$yield_total because it doesn't seem to do anything?
+echo `python -c "print float($yieldnum - $noyieldnum)/(2*$n)"`
