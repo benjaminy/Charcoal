e9cb494336e855acd956fef0bf6bed3f2a2ca9a1
Jacqueline Kay <jkay1@cs.swarthmore.edu>
2013-11-26 22:56:03 -0500

    Copied graphing script changes from EfficientYield.

--- Testing/MicroTests/graphs.py
+++ Testing/MicroTests/graphs.py
@@ -4,16 +4,26 @@ import pylab
 
 def main():
 
-  f = open('simple-yield-small.txt', 'rb')
+  f = open('simple-yield.txt', 'rb')
   nums = []
   for line in f:
-    print line
     nums.append(float(line))
-  pylab.plot(range(1,6), nums, 'mo')
+  pylab.plot(range(1,101), nums, 'mo')
   pylab.axis([0, 101, 0, 10]) # xmin, xmax, ymin, ymax
   pylab.xlabel('Iteration')
-  pylab.ylabel('Yield time')
+  pylab.ylabel('Yield time (microsec)')
   pylab.show()
+
+  unyield = open('simple-unyield.txt', 'rb')
+  n = []
+  for line in unyield:
+    n.append(float(line))
+  pylab.plot(range(1,101), n, 'bo')
+  pylab.axis([0, 101, 0, 1]) # xmin, xmax, ymin, ymax
+  pylab.xlabel('Iteration')
+  pylab.ylabel('Yield time (microsec)')
+  pylab.show()
+ 
 
 
   return 0
