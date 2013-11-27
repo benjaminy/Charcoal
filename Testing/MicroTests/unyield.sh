a4b5d86a03436f7c9490100760186ba13826c344
Joshua Gluck <jgluck2@cs.swarthmore.edu>
2013-11-26 21:45:49 -0500

    Fixed unyield.sh

--- Testing/MicroTests/unyield.sh
+++ Testing/MicroTests/unyield.sh
@@ -3,20 +3,14 @@ make clean
 make simple_unyield_test
 make simple_unyield_test_noyield
 n=1000000
-numruns=1000000000
-yield_total=0
+numruns=20
 yieldnum=0
 noyieldnum=0
-for j in {1..$numruns}; do
-		out=`./simple_unyield_test $n`
-		yieldnum=`expr $yieldnum + $out`
+
+for j in {1..100}
+do
+        yieldnum=`./simple_unyield_test $n`
+        noyieldnum=`./simple_unyield_test_noyield $n`
+        echo `python -c "print float($yieldnum - $noyieldnum)/(2*$n)"`
 done
 
-echo $yieldnum
-for j in {1..$numruns}; do
-		out=`./simple_unyield_test_noyield $n`
-		noyieldnum=`expr $noyieldnum + $out`
-done
-echo $noyieldnum
-#Deleted +$yield_total because it doesn't seem to do anything?
-echo `python -c "print float($yieldnum - $noyieldnum)/(2*$n)"`
