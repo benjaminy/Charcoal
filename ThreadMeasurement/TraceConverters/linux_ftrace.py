350f735b3da79f8b7a1d4c82fcac408b9a626331
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2014-02-24 13:19:22 -0800

    Ftrace converter in pretty good shape

--- ThreadMeasurement/TraceConverters/linux_ftrace.py
+++ ThreadMeasurement/TraceConverters/linux_ftrace.py
@@ -6,9 +6,77 @@ format.  This program reads the input file and converts it into the
 thread measurement project's simple sqlite format.'''
 
 import argparse, subprocess, tempfile, re, sqlite3, os
+import thread_study_utils as utils
+
+def translate_ftrace_to_generic():
+    args = parse_command_line_args()
+    temp = tempfile.NamedTemporaryFile(mode='w+b')
+    dat_to_txt(args.dat_file, temp.name)
+    try:
+        os.remove(args.sql_file)
+    except:
+        pass  # Really doesn't matter
+    conn = sqlite3.connect(args.sql_file)
+    c = conn.cursor()
+    utils.init_sqlite_file(c)
+
+    pat = re.compile('-.*\[(.*)\]\D*(\d*)\.(\d*).*sched_switch\D*(\d*).*==>\D*(\d*)\S*\s+(.*)')
+    min_time = min_timestamp(temp, pat)
+    temp.seek(0)
+    threads = {}
+    processes = {}
+    # Assumption: The pid of the idle task is 0
+    threads[0] = 0
+    event_id = 1
+    process_id = 1
+    for line in temp:
+        m = pat.search(line)
+        if not m:
+            print 'NO MATCH',line,
+            continue
+        cpu           = int(m.group(1))
+        time_sec      = int(m.group(2))
+        time_usec     = int(m.group(3))
+        from_pid      = int(m.group(4))
+        to_pid        = int(m.group(5))
+        to_task_name  = m.group(6)
+        time_ns_abs = ((time_sec * 1000000) + time_usec) * 1000
+        time_ns = time_ns_abs - min_time
+        print('CPU:%d stamp:%12d from:%8d to:%8d  to_name:%s' %
+              (cpu, time_ns, from_pid, to_pid, to_task_name))
+        # Deal with the thread that's stopping
+        if from_pid != 0 and (from_pid in threads):
+            c.execute("INSERT INTO events VALUES (%d, %d, %d, 2, %d)" %
+                      (event_id, from_pid, cpu, time_ns))
+            event_id += 1
+        # Deal with the thread that's starting
+        process_id = utils.ensure_thread_in_db(
+            to_pid, to_task_name, to_task_name, threads, processes, c, process_id)
+        if to_pid != 0:
+            c.execute("INSERT INTO events VALUES (%d, %d, %d, 1, %d)" %
+                      (event_id, to_pid, cpu, time_ns))
+            event_id += 1
+
+    # Save (commit) the changes
+    conn.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
+
+def parse_command_line_args():
+    argp = argparse.ArgumentParser(description='''Ftrace to generic trace translator''')
+    argp.add_argument('dat_file', metavar='T',
+                      help='Name of the input ftrace dat file')
+    argp.add_argument('sql_file', metavar='G',
+                      help='Name of the output sqlite3 file')
+    return argp.parse_args()
 
 def dat_to_txt(in_name, out_name):
+    ''' dat_to_txt is just a thin wrapper around `trace-cmd report` '''
     try:
+        # Doesn't seem like the most elegant way to invoke another
+        # program, but it works
         cmd = ['trace-cmd report -i '+in_name+' > '+out_name]
         print cmd
         return_code = subprocess.call(cmd, shell=True)
@@ -21,113 +89,18 @@ def dat_to_txt(in_name, out_name):
         print 'Command just flat out failed'
         raise
 
-def init_sqlite_file(c):
-    tables = []
-    tables.append(('event_kinds',
-        ['id INTEGER PRIMARY KEY', 'name TEXT']))
-    tables.append(('processes',
-        ['id INTEGER PRIMARY KEY', 'name TEXT', 'friendly_name TEXT']))
-    tables.append(('threads',
-        ['id INTEGER PRIMARY KEY', 'name TEXT', 'process INTEGER',
-         'FOREIGN KEY(process) REFERENCES processes(id)']))
-    evs_table = []
-    evs_table.append('id INTEGER PRIMARY KEY')
-    evs_table.append('thread INTEGER')
-    evs_table.append('core INTEGER')
-    evs_table.append('kind INTEGER')
-    evs_table.append('timestamp INTEGER')
-    evs_table.append('FOREIGN KEY(thread) REFERENCES threads(id)')
-    evs_table.append('FOREIGN KEY(kind) REFERENCES event_kinds(id)')
-    tables.append(('events', evs_table ))
-
-    for (name, columns) in tables:
-        cmd = 'CREATE TABLE '+name+'\n    ('
-        first = True
-        for column in columns:
-            if not first:
-                cmd += ', '
-            cmd += column
-            first = False
-        cmd += ')'
-        print cmd
-        c.execute(cmd)
-
-    c.execute("INSERT INTO event_kinds VALUES (1,'start')")
-    c.execute("INSERT INTO event_kinds VALUES (2,'stop')")
-
-
-def translate_ftrace_to_generic():
-    argp = argparse.ArgumentParser(description='''Ftrace to generic trace translator''')
-    argp.add_argument('dat_file', metavar='T',
-                      help='Name of the input ftrace dat file')
-    argp.add_argument('sql_file', metavar='G',
-                      help='Name of the output sqlite3 file')
-    args = argp.parse_args()
-
-    temp = tempfile.NamedTemporaryFile(mode='w+b')
-
-    dat_to_txt(args.dat_file, temp.name)
-
-    try:
-        os.remove(args.sql_file)
-    except:
-        pass  # Really doesn't matter
-    conn = sqlite3.connect(args.sql_file)
-    c = conn.cursor()
-
-    init_sqlite_file(c)
-
-    threads = {}
-    processes = {}
-    threads[0] = 0
-    pat = re.compile('-.*\[(.*)\]\D*(\d*)\.(\d*).*sched_switch\D*(\d*).*==>\D*(\d*)\S*\s+(.*)')
-    event_id = 1
-    process_id = 1
-    for line in temp:
+def min_timestamp(lines, pat):
+    min_time = -1
+    for line in lines:
         m = pat.search(line)
-        if m:
-            cpu_txt       = m.group(1)
-            time_sec_txt  = m.group(2)
-            time_usec_txt = m.group(3)
-            from_pid_txt  = m.group(4)
-            to_pid_txt    = m.group(5)
-            to_task_name  = m.group(6)
-            cpu       = int(cpu_txt)
-            time_sec  = int(time_sec_txt)
-            time_usec = int(time_usec_txt)
-            from_pid  = int(from_pid_txt)
-            to_pid    = int(to_pid_txt)
-            time = ((time_sec * 1000000) + time_usec) * 1000
-            print('CPU:%d stamp:%d from:%8d to:%8d  to_name:%s' %
-                  (cpu, time, from_pid, to_pid, to_task_name))
-            if from_pid != 0 and (from_pid in threads):
-                c.execute("INSERT INTO events VALUES (%d, %d, %d, 2, %d)" %
-                          (event_id, from_pid, cpu, time))
-                event_id += 1
-            if not to_pid in threads:
-                if not to_task_name in processes:
-                    c.execute("INSERT INTO processes VALUES (%d, '%s', '%s')" %
-                              (process_id, to_task_name, to_task_name))
-                    processes[to_task_name] = process_id
-                    process_id += 1
-                to_task_id = processes[to_task_name]
-                c.execute("INSERT INTO threads VALUES (%d, '%d', %d)" %
-                          (to_pid, to_pid, to_task_id))
-                threads[to_pid] = to_task_name
-                    
-            if to_pid != 0:
-                c.execute("INSERT INTO events VALUES (%d, %d, %d, 1, %d)" %
-                          (event_id, to_pid, cpu, time))
-                event_id += 1
-        else:
-            print 'NO MATCH',line,
-
-    # Save (commit) the changes
-    conn.commit()
-
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
-    conn.close()
+        if not m:
+            continue
+        time_sec      = int(m.group(2))
+        time_usec     = int(m.group(3))
+        time_ns = ((time_sec * 1000000) + time_usec) * 1000
+        if (min_time < 0) or (time_ns < min_time):
+            min_time = time_ns
+    return min_time
 
 if __name__ == '__main__':
     translate_ftrace_to_generic()
