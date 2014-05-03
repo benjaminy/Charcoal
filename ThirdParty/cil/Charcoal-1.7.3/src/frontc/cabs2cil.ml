7b99b07044e91c1ee6a66e72644a074c8cf7959c
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2014-05-03 07:56:36 -0400

    Merged Cil changes

--- ThirdParty/cil/Charcoal-1.7.3/src/frontc/cabs2cil.ml
+++ ThirdParty/cil/Charcoal-1.7.3/src/frontc/cabs2cil.ml
@@ -6664,83 +6664,70 @@ end
 
 let stripParenFile file = V.visitCabsFile (new stripParenClass) file
 
-(* Exctract activity bodies out into top-level procedures *)
-
-(* module E = Errormsg *)
-(* module T = Trace *)
-(* module P = Pretty *)
-(*module C = Cabs*)
-(* module V = Cabsvisit *)
+(* Extract activity bodies out into top-level procedures *)
 
 (* NOTE: For nested activities, if the inner uses a by-ref from way out,
    add the new var name to the inner activate's by-val list. *)
 
-(* class extract_activity_class : V.cabsVisitor = *)
-(* object (self) *)
-(*   inherit V.nopCabsVisitor as super *)
+class extract_activity_class : V.cabsVisitor =
+object (self)
+  inherit V.nopCabsVisitor as super
 
-(*   val mutable activate_depth = 0 *)
-(*   val mutable fun_def_depth = 0 *)
+  val mutable activate_depth = 0
+  val mutable fun_def_depth = 0
 
-(*   method vdef d : C.definition list V.visitAction = match d with *)
-(*       C.FUNDEF(fname, body, loc, lend) -> *)
-(*         let after defs = *)
-(*           let () = fun_def_depth <- fun_def_depth - 1 in *)
-(*           match defs with *)
-(*               [def] -> defs *)
-(*             | _ -> failwith "wrong number of defs" *)
-(*         in *)
-(*         (\* XXX add the parameters to the local defs *\) *)
-(*         let () = fun_def_depth <- fun_def_depth + 1 in *)
-(*         V.ChangeDoChildrenPost ([d], after) *)
-(*     | C.DECDEF (name_group, loc) -> V.DoChildren *)
-(*       (\* XXX: I guess there might be typedefs and shit like that *\) *)
-(*     | _ -> V.DoChildren *)
+  method vdef d : definition list V.visitAction = match d with
+      FUNDEF(fname, body, loc, lend) ->
+        let after defs =
+          let () = fun_def_depth <- fun_def_depth - 1 in
+          match defs with
+              [def] -> defs
+            | _ -> failwith "wrong number of defs"
+        in
+        (* XXX add the parameters to the local defs *)
+        let () = fun_def_depth <- fun_def_depth + 1 in
+        V.ChangeDoChildrenPost ([d], after)
+    | DECDEF (name_group, loc) -> V.DoChildren
+      (* XXX: I guess there might be typedefs and shit like that *)
+    | _ -> V.DoChildren
 
-(*   method vexpr e = match e with *)
-(*       C.ACTIVATE (activity, by_val_vars, stmt_body) -> *)
-(*         if activate_depth > 0 then V.DoChildren *)
-(*         else V.DoChildren *)
-(*     | C.VARIABLE v -> *)
-(*       if activate_depth > 0 then *)
-(*         match None (\* XXX *\) with *)
-(*             None       -> V.SkipChildren *)
-(*           | Some _ -> V.ChangeTo (C.UNARY (C.MEMOF, e)) *)
-(*       else *)
-(*         V.SkipChildren *)
-(*     | _ -> V.DoChildren *)
+  method vexpr e = match e with
+      ACTIVATE (activity, by_val_vars, stmt_body) ->
+        if activate_depth > 0 then V.DoChildren
+        else V.DoChildren
+    | VARIABLE v ->
+      if activate_depth > 0 then
+        match None (* XXX *) with
+            None       -> V.SkipChildren
+          | Some _ -> V.ChangeTo (UNARY (MEMOF, e))
+      else
+        V.SkipChildren
+    | _ -> V.DoChildren
 
-(*   method vEnterScope () = () *)
-(*   method vExitScope  () = () *)
+  method vEnterScope () = ()
+  method vExitScope  () = ()
 
-(* end (\* extract_activity_class *\) *)
+end (* extract_activity_class *)
 
-(*let rec extract_one def : C.definition list option =
+let rec extract_one def : definition list option =
   None
 
-let rec extract_all defs : C.definition list =
+let rec extract_all defs : definition list =
   match defs with
       [] -> []
     | def::rest ->
       (match extract_one def with
           None -> def::(extract_all defs)
         | Some new_defs -> (extract_all new_defs) @ (extract_all rest))
-*)
-let activity_extract cabs : (string * definition list) =
-  cabs
-(*  let foo,defs = cabs in
-  let defs' = extract_all defs in
-  foo,defs'
-*)
-
 
 (* Translate a file *)
 let convFile (f : A.file) : Cil.file =
   Cil.initCIL (); (* make sure we have initialized CIL *)
 
   (* remove parentheses from the Cabs *)
-  let fname,dl = stripParenFile f in 
-    
+  let fname,dl_with_activate = stripParenFile f in 
+  let dl = extract_all dl_with_activate in
+
   (* Clean up the global types *)
   initGlobals();
   startFile ();
