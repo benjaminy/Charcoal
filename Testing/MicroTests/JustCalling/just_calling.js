660cec5a51b5286549cf30fed57e0f698e13e072
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2015-01-05 10:43:34 -0700

    jc

---
+++ Testing/MicroTests/JustCalling/just_calling.js
@@ -0,0 +1,43 @@
+
+function f01(a) { return (a * 17 + 29) % 43; }
+
+function f02(a) { return f01( f01( a ) ); }
+function f03(a) { return f02( f02( a ) ); }
+function f04(a) { return f03( f03( a ) ); }
+function f05(a) { return f04( f04( a ) ); }
+function f06(a) { return f05( f05( a ) ); }
+function f07(a) { return f06( f06( a ) ); }
+function f08(a) { return f07( f07( a ) ); }
+function f09(a) { return f08( f08( a ) ); }
+function f10(a) { return f09( f09( a ) ); }
+function f11(a) { return f10( f10( a ) ); }
+function f12(a) { return f11( f11( a ) ); }
+function f13(a) { return f12( f12( a ) ); }
+function f14(a) { return f13( f13( a ) ); }
+function f15(a) { return f14( f14( a ) ); }
+function f16(a) { return f15( f15( a ) ); }
+function f17(a) { return f16( f16( a ) ); }
+function f18(a) { return f17( f17( a ) ); }
+function f19(a) { return f18( f18( a ) ); }
+function f20(a) { return f19( f19( a ) ); }
+function f21(a) { return f20( f20( a ) ); }
+function f22(a) { return f21( f21( a ) ); }
+function f23(a) { return f22( f22( a ) ); }
+function f24(a) { return f23( f23( a ) ); }
+function f25(a) { return f24( f24( a ) ); }
+function f26(a) { return f25( f25( a ) ); }
+function f27(a) { return f26( f26( a ) ); }
+function f28(a) { return f27( f27( a ) ); }
+function f29(a) { return f28( f28( a ) ); }
+function f30(a) { return f29( f29( a ) ); }
+function f31(a) { return f30( f30( a ) ); }
+function f32(a) { return f31( f31( a ) ); }
+function f33(a) { return f32( f32( a ) ); }
+function f34(a) { return f33( f33( a ) ); }
+function f35(a) { return f34( f34( a ) ); }
+function f36(a) { return f35( f35( a ) ); }
+function f37(a) { return f36( f36( a ) ); }
+function f38(a) { return f37( f37( a ) ); }
+function f39(a) { return f38( f38( a ) ); }
+
+console.log( f30( 42 ) );
