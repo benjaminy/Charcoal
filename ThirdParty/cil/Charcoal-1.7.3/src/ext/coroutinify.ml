bdde3b7a7f8ecbe76ff192c83d9a3f1f69363312
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2015-02-09 07:23:25 -0700

    Added coroutinification of return statements.  Also cleaned things up a little.

--- ThirdParty/cil/Charcoal-1.7.3/src/ext/coroutinify.ml
+++ ThirdParty/cil/Charcoal-1.7.3/src/ext/coroutinify.ml
@@ -20,7 +20,7 @@ let mkSimpleField ci fn ft fl =
   { C.fcomp = ci ; C.fname = fn ; C.ftype = ft ; C.fbitfield = None ; C.fattr = [];
     C.floc = fl }
 
-let change_do_children_no_post x = C.ChangeDoChildrenPost( x, fun e -> e )
+let change_do_children x = C.ChangeDoChildrenPost( x, fun e -> e )
 
 let label_counter = ref 1
 let fresh_return_label loc =
@@ -79,7 +79,7 @@ class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
     match lhost with
       C.Var v ->
       ( match IH.tryfind locals v.C.vid with
-          Some e -> change_do_children_no_post( C.Mem e, offset )
+          Some e -> change_do_children( C.Mem e, offset )
         | None -> C.DoChildren )
     (* We can ignore the Mem case because we'll get it later in the visit *)
     | _ -> C.DoChildren
@@ -89,23 +89,26 @@ class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
    * returns and labels, so we need the method that lets us return
    * statements. *)
   method vstmt s =
-    let ( frame_type, frame_var, frame_return_field, frame_locals_field, frame_callee_field ) =
+    let ( frame_type, frame_var, frame_return_field, frame_locals_field,
+          frame_callee_field, frame_caller_field ) =
       frame_info
     in
+    let frame_lval = C.var frame_var in
+    let frame_exp = C.Lval( frame_lval ) in
     let is_not_call i =
       match i with
         C.Call _ -> false
       | _ -> true
     in
     (* Translate calls from:
-     *   lhs = f( p1, p2, p3 );
+     *     lhs = f( p1, p2, p3 );
      * to:
-     *   frame->return_pointer = &__charcoal_return_N;
-     *   return __charcoal_fn_init_f( frame, p1, p2, p3 );
+     *     frame->return_pointer = &__charcoal_return_N;
+     *     return __charcoal_fn_init_f( frame, p1, p2, p3 );
      *   __charcoal_return_N:
-     *   lhs = *( ( return type * ) &( frame->callee->locals ) );
-     *   free( frame->callee );
-     *   frame->callee = 0xJUNK; /* TODO: maybe add for debugging */
+     *     lhs = *( ( return type * ) &( frame->callee->locals ) );
+     *     free( frame->callee );
+     *     frame->callee = 0xJUNK; /* TODO: maybe add for debugging */
      *)
     let coroutinify_call instr =
       match instr with
@@ -114,7 +117,6 @@ class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
             C.Lval( C.Var v, C.NoOffset ) -> fooo v
           | _ -> E.s( E.unimp "Oh noes; indirect calls" )
         in
-        let frame_exp = C.Lval( C.var frame_var ) in
         let callee_lval = ( C.Mem frame_exp, C.Field( frame_callee_field, C.NoOffset ) ) in
         let return =
           let after =
@@ -130,8 +132,8 @@ class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
                   C.TFun( r, _, _, _ ) -> r
                 | _ -> E.s( E.error "Function with non-function type?!?" )
               in
-              let lh = C.Mem( C.mkCast ( C.mkAddrOf locals_lval ) return_type ) in
-              [ C.Set( lhs, C.Lval( lh, C.NoOffset ), loc ); free_callee ]
+              let lhost = C.Mem( C.mkCast ( C.mkAddrOf locals_lval ) return_type ) in
+              [ C.Set( lhs, C.Lval( lhost, C.NoOffset ), loc ); free_callee ]
             | _ -> [ free_callee ]
           in
           let r = C.mkStmt( C.Instr after ) in
@@ -150,15 +152,42 @@ class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
         in
         let init_return = C.mkStmt( C.Return( Some( C.Lval temp_callee ), loc ) )  in
         [save_goto; init_call; init_return; return]
-      | _ -> [C.mkStmt( Instr[ instr ] )]
+      | _ -> [C.mkStmt( C.Instr[ instr ] )]
     in
-    match s.skind with
-      (Instr instrs) ->
+
+    (* Translate returns from:
+     *     return exp;
+     * to:
+     *     *( ( return type * ) &( frame->locals ) ) = exp;
+     *     return frame->caller;
+     *)
+    let coroutinify_return rval_opt loc =
+      let caller_lval = ( C.Mem frame_exp, C.Field( frame_caller_field, C.NoOffset ) ) in
+      let return_stmt = C.mkStmt( C.Return( Some( C.Lval caller_lval ), loc ) ) in
+      (match rval_opt with
+       | None -> change_do_children return_stmt
+       | Some rval ->
+          let locals_lval =
+            ( C.Mem( C.Lval frame_lval ), C.Field( frame_locals_field, C.NoOffset ) )
+          in
+          let return_type =
+            match fdec.C.svar.C.vtype with
+              C.TFun( r, _, _, _ ) -> r
+            | _ -> E.s( E.error "Function with non-function type?!?" )
+          in
+          let lhs = ( C.Mem( C.mkCast ( C.mkAddrOf locals_lval ) return_type ), C.NoOffset ) in
+          let set_return = C.mkStmt( C.Instr[ C.Set( lhs, rval, loc ) ] ) in
+          change_do_children( C.mkStmt( C.Block( C.mkBlock[ set_return; return_stmt ] ) ) ) )
+    in
+
+    match s.C.skind with
+      (C.Instr instrs) ->
       if List.for_all is_not_call instrs then
         C.DoChildren
       else
         let stmts = List.concat (List.map coroutinify_call instrs) in
-        C.DoChildren
+        change_do_children( C.mkStmt( C.Block( C.mkBlock stmts ) ) )
+    | (C.Return( rval_opt, loc )) -> coroutinify_return rval_opt loc
     | _ -> C.DoChildren
 end
 
@@ -166,12 +195,12 @@ class findFunctionsVisitor = object(self)
   inherit C.nopCilVisitor
   method vglob g =
     match g with
-    | GFun( fdec, loc ) ->
+    | C.GFun( fdec, loc ) ->
        let kind = crcl_fun_kind fdec.C.svar.C.vname in
        (match kind with
           CRCL_FUN_OTHER ->
           let yielding_version = C.copyFunction fdec in
-          change_do_children_no_post [g]
+          change_do_children [g]
         | _ -> C.SkipChildren
        )
     | _ -> C.SkipChildren
