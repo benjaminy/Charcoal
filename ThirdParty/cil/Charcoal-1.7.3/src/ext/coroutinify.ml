b108bcdc4dd231a22dc29fe0488205e943b9f930
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2015-02-10 10:59:50 -0700

    Another little chunk of making the yielding version.

--- ThirdParty/cil/Charcoal-1.7.3/src/ext/coroutinify.ml
+++ ThirdParty/cil/Charcoal-1.7.3/src/ext/coroutinify.ml
@@ -16,6 +16,8 @@ module C  = Cil
 module E  = Errormsg
 module IH = Inthash
 
+let spf = Printf.sprintf
+
 let mkSimpleField ci fn ft fl =
   { C.fcomp = ci ; C.fname = fn ; C.ftype = ft ; C.fbitfield = None ; C.fattr = [];
     C.floc = fl }
@@ -26,7 +28,7 @@ let label_counter = ref 1
 let fresh_return_label loc =
   let n = !label_counter in
   let () = label_counter := n + 1 in
-  C.Label( Printf.sprintf "__charcoal_return_%d" n, loc, false )
+  C.Label( spf "__charcoal_return_%d" n, loc, false )
 
 type crcl_fun_kind =
   | CRCL_FUN_YIELDING
@@ -37,16 +39,27 @@ type crcl_fun_kind =
 let crcl_yielding_prefix   = "__charcoal_fn_yielding_"
 let crcl_unyielding_prefix = "__charcoal_fn_unyielding_"
 let crcl_init_prefix       = "__charcoal_fn_init_"
-let crcl_yielding_prefix_len   = String.length crcl_yielding_prefix
-let crcl_unyielding_prefix_len = String.length crcl_unyielding_prefix
-let crcl_init_prefix_len       = String.length crcl_init_prefix
+let crcl_locals_prefix     = "__charcoal_fn_locals_"
+let (crcl_yielding, crcl_unyielding, crcl_init, crcl_locals) =
+  let ps = [ crcl_yielding_prefix; crcl_unyielding_prefix;
+             crcl_init_prefix; crcl_locals_prefix; ]
+  in
+  let ps_lens = List.map ( fun p -> ( p, String.length p ) ) ps in
+  let comp( p, l ) =
+    fun s ->
+    let m = ( p = Str.string_before s l ) in
+    ( m, if m then Str.string_after s l else "" )
+  in
+  ( match List.map comp ps_lens with
+      [y;u;i;l] -> (y,u,i,l)
+    | _ -> E.s( E.error "Unpossible!!!" ) )
 
 let crcl_fun_kind name =
-  if crcl_yielding_prefix = Str.string_before name crcl_yielding_prefix_len then
+  if fst( crcl_yielding name ) then
     CRCL_FUN_YIELDING
-  else if crcl_unyielding_prefix = Str.string_before name crcl_unyielding_prefix_len then
+  else if fst( crcl_unyielding name ) then
     CRCL_FUN_UNYIELDING
-  else if crcl_init_prefix = Str.string_before name crcl_init_prefix_len then
+  else if fst( crcl_init name ) then
     CRCL_FUN_INIT
   else
     CRCL_FUN_OTHER
@@ -63,11 +76,12 @@ let function_vars = IH.create 42
 let fooo (v : C.varinfo) =
   try IH.find function_vars v.C.vid
   with Not_found ->
-       let i = C.makeVarinfo true (crcl_init_prefix       ^ v.C.vname) C.voidType in
-       let y = C.makeVarinfo true (crcl_yielding_prefix   ^ v.C.vname) C.voidType in
-       let u = C.makeVarinfo true (crcl_unyielding_prefix ^ v.C.vname) C.voidType in
-       let () = IH.replace function_vars v.C.vid (i, y, u) in
-       (i, y, u)
+    (* XXX fix types: *)
+    let i = C.makeVarinfo true (spf "%s%s" crcl_init_prefix       v.C.vname) C.voidType in
+    let y = C.makeVarinfo true (spf "%s%s" crcl_yielding_prefix   v.C.vname) C.voidType in
+    let u = C.makeVarinfo true (spf "%s%s" crcl_unyielding_prefix v.C.vname) C.voidType in
+    let () = IH.replace function_vars v.C.vid (i, y, u) in
+    (i, y, u)
 
 class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
   inherit C.nopCilVisitor
@@ -104,11 +118,13 @@ class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
      *     lhs = f( p1, p2, p3 );
      * to:
      *     frame->return_pointer = &__charcoal_return_N;
+     *     /* allocates callee frame: */
      *     return __charcoal_fn_init_f( frame, p1, p2, p3 );
      *   __charcoal_return_N:
      *     lhs = *( ( return type * ) &( frame->callee->locals ) );
      *     free( frame->callee );
-     *     frame->callee = 0xJUNK; /* TODO: maybe add for debugging */
+     *     /* TODO: maybe add for debugging: */
+     *     frame->callee = 0xJUNK;
      *)
     let coroutinify_call instr =
       match instr with
@@ -191,6 +207,63 @@ class coroutinifyYieldingVisitor fdec locals frame_info free_fn = object(self)
     | _ -> C.DoChildren
 end
 
+(* Translate from:
+ *     rt f( p1, p2, p3 ) { ... }
+ * to:
+ *     frame_p __charcoal_fn_init_f( frame_p caller, p1, p2, p3 )
+ *     {
+ *        size_t locals_size = sizeof( f struct );
+ *        /* using a call here to reduce explosion down: */
+ *        frame_p frame = __crcl_generic_init( caller, locals_size, __crcl_fn_yielding_f );
+ *        ( f struct ) *locals = &frame->locals;
+ *        locals->p1 = p1;
+ *        locals->p2 = p2;
+ *        locals->p3 = p3;
+ *        locals->l1 = init1;
+ *        locals->l2 = init2;
+ *        locals->l3 = init3;
+ *        return frame;
+ *     }
+ *)
+let make_init_version () =
+  ()
+
+let make_yielding_version fdec =
+  let new_name = spf "%s%s" crcl_yielding_prefix fdec.C.svar.C.vname in
+  let yielding_version = C.copyFunction fdec new_name in
+  let fields =
+    let f v = (v.C.vname, v.C.vtype, None (* bitfield *), [(*attrs*)], v.C.vdecl) in
+    (List.map f fdec.C.sformals) @ (List.map f fdec.C.slocals)
+  in
+  let struct_name = spf "%s%s" crcl_locals_prefix fdec.C.svar.C.vname in
+  let locals_struct = C.mkCompInfo true struct_name (fun _ -> fields) [(* attrs *)] in
+  let locals_type = C.TPtr( C.TComp( locals_struct, [(*attrs*)] ), [(*attrs*)] ) in
+  let locals_init =
+    C.SingleInit( C.SizeOf locals_type (* XXX & ( *frame . locals ) *) ) in
+  let locals_var =
+    C.makeLocalVar yielding_version "locals" ~init:locals_init locals_type
+  in
+  let locals_tbl = IH.create (List.length fields) in
+  let () =
+    let make_field v =
+      (* XXX don't lose inits *)
+      let f =
+        { C.fcomp     = locals_struct;
+          C.ftype     = v.C.vtype;
+          C.fname     = v.C.vname;
+          C.fbitfield = None;
+          C.fattr     = [];
+          C.floc      = v.C.vdecl }
+      in
+      let l = C.AddrOf( C.Mem( C.Lval( C.var locals_var ) ),
+                        C.Field( f, C.NoOffset ) )
+      in
+      IH.replace locals_tbl v.C.vid l
+    in
+    List.iter make_field ( fdec.C.sformals @ fdec.C.slocals )
+  in
+  yielding_version
+
 class findFunctionsVisitor = object(self)
   inherit C.nopCilVisitor
   method vglob g =
@@ -199,10 +272,18 @@ class findFunctionsVisitor = object(self)
        let kind = crcl_fun_kind fdec.C.svar.C.vname in
        (match kind with
           CRCL_FUN_OTHER ->
-          let yielding_version = C.copyFunction fdec in
+          let xxx = make_yielding_version fdec in
           change_do_children [g]
         | _ -> C.SkipChildren
        )
+
+    (* We need to find various struct definitions *)
+    | C.GCompTag( compinfo, location ) ->
+       (* XXX NO NO NO. We make the locals *)
+       let (is_locals, locals_name) = crcl_locals compinfo.C.cname in
+       C.SkipChildren
+       (*  if crcl_yielding_prefix = Str.string_before name crcl_yielding_prefix_len then*)
+
     | _ -> C.SkipChildren
 end
 
