3bf70a8e1c11780ac23775e93f34cc70f23d75df
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2015-07-01 10:58:47 -0600

    rename

--- Testing/MicroTests/JustCalling/just_calling_rec.c
+++ Testing/MicroTests/JustCalling/just_calling_rec.c
@@ -1,19 +1,13 @@
 int A[ 9 ] = { 4, 8, 3, 5, 6, 7, 2, 1, 0 };
 
-#define N 50000000
+#define N 100000000
 
 int f( int n, int x )
 {
     if( n < 2 )
-    {
         return A[ x ];
-    }
     else
-    {
-        int half1 = n / 2;
-        int half2 = n - half1;
-        return f( half1, f( half2, x ) );
-    }
+        return f( ( n + 1 ) / 2, f( n / 2, x ) );
 }
 
 int main( int argc, char **argv )
