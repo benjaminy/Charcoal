bac7fa0e732a06fccab677959deb41eb6d013c79
Miguel Guerrero <Miguel Guerrero@192.168.56.1>
2017-06-29 15:01:39 -0600

    Working on the profiler scanner to get a deeper report

--- RealAppAnalysis/profile_scanner.py
+++ RealAppAnalysis/profile_scanner.py
@@ -5,9 +5,22 @@ import datautil
     
 def getCPUProfileNodes(data): return data[-1]["args"]["data"]["cpuProfile"]["nodes"]
 def log(str, indent = 1): print(("\t" * indent) + str) 
+def trimFunctionCallEvent(fcall_event):
+    del(fcall_event["ts"])
+    del(fcall_event["ph"])
+    del(fcall_event["cat"])
+    fcall_event["name"] = fcall_event["args"]["data"]["functionName"]
+    del(fcall_event["args"])
+    
+def trimCPUNode(node, pid = 0, tid = 0):
+    {"pid": pid, "tid": tid, "name": node["callFrame"]["functionName"]}
+    del(node["callFrame"])
+    del(node["hitCount"])
+    del(node["id"])
 
+    
 def onAllDataInDir(root_dir, func):
-    isDataFile = lambda filename: filename.endswith(".json")
+    def isDataFile(filename): return filename.endswith(".json")
     for (filepath, sub_directories, files) in walk(root_dir):
         print("Filepath: " + filepath)
         print("Subdirectories: " + str(sub_directories))
@@ -18,12 +31,12 @@ def onAllDataInDir(root_dir, func):
                 try:
                     with open((join(filepath, file)), 'r') as data_file: 
                         data = load(data_file)
-
                 except:
-                    print("Error in processing data file: " + file)      
-                    
-            func(data)         
+                    print("Error in processing data file: " + file)  
+                    data = []    
     
+            func(data)
+                     
 def examineProfileAccuracy(data):
     cpu_profile = data[-1]
     fcall_count_in_cpuprofile = len(cpu_profile["args"]["data"]["cpuProfile"]["nodes"])
@@ -36,6 +49,20 @@ def examineProfileAccuracy(data):
     print("Number of function calls")
     print("CPU Profile: %d Data: %d" % (fcall_count_in_cpuprofile, fcall_count_in_data))
     
+    
+def compareCPUProfileToFunctionEvents2(data):
+    """Work in progress"""
+    cpu_profile = data[-1]
+    
+    cpup_nodes = getCPUProfileNodes(data)
+    for node in cpup_nodes: 
+        trimCPUNode(node, pid = cpu_profile["pid"], tid = cpu_profile["tid"])
+    
+    fcall_events = datautil.filterEvents(data, "name", "FunctionCall")
+    for fcall_event in fcall_events:
+        trimFunctionCallEvent(fcall_event)
+    
+    print(cpup_nodes)
     
 def compareCPUProfileToFunctionEvents(data):
     cpu_profile_nodes = getCPUProfileNodes(data)
