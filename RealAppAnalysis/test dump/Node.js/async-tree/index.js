d42ea943c1154e1c5837ca05ae90aa3236f0acc8
Miguel Angel Guerrero <miguel_angel_g@live.com>
2017-07-18 19:42:43 -0600

    Console.log is async... also IDs

--- RealAppAnalysis/test dump/Node.js/async-tree/index.js
+++ RealAppAnalysis/test dump/Node.js/async-tree/index.js
@@ -1,51 +1,53 @@
 'use strict';
 
-if(!process.addAsyncListener)
+if(!process.addAsyncListener){
     require('async-listener');
-
-var stack = require('./lib/stack');
-
-
-Error.stackTraceLimit = Infinity;
-
-var stackSeparator = 'From previous event:';
-var currentStack = null;
-
+}
 
 /**
  * AsyncListener object
  */
 var AsyncListener = {};
 
+var ID = 0;
+var tree = [];
 
-AsyncListener.create = function create() {
-    // This always gets called in between before/after
-    var trace = {};
-    Error.captureStackTrace(trace, create);
-    trace.name = stackSeparator;
-    return stack(trace.stack, currentStack);
+/**
+Called when an asynchronous event is queued. Recieves the storage attached to the listener. 
+Storage can be created by passing an initialStorage argument during costruction, 
+or by returning a Value from create which will be attached to the listner and overwrite the initialStorage.
+**/
+
+AsyncListener.create = function create(storage) {
+};
+
+/**
+A function (context, storage) that is called immediately before the asynchronous callback is about to run. 
+It will be passed both the context (i.e. this) of the calling function and the storage.
+**/
+AsyncListener.before = function before(context, storage) {
+	ID++;
 };
 
 
-AsyncListener.before = function before(context, stack) {
-    currentStack = stack;
+/**
+A function (context, storage) called immediately after the asynchronous event's callback is run. 
+Note that if the event's callback throws during execution this will not be called.
+**/
+AsyncListener.after = function after(context, storage) {
 };
 
-
-AsyncListener.after = function after() {
-    currentStack = null;
+/**
+A function (storage, error) called if the event's callback threw. 
+If error returns true then Node will assume the error has been properly handled and resume execution normally.
+**/
+AsyncListener.error = function error(storage, error){
 };
-
-
-AsyncListener.error = function error(stack, err) {
-    if (stack) {
-        err.stack += '\n' + stack.getStacks().join('\n');
-    }
-};
-
 
 var key = process.addAsyncListener(AsyncListener);
+function printID(){console.log(ID);}
+process.on('exit', printID);
+exports.module = key;
 
 
-module.exports = key;
-module.exports.stackSeparator = stackSeparator;
+
