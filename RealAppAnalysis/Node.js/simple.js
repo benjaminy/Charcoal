63db6b90bb9a3079871171e8d14d1ed5fe03f147
Miguel Angel Guerrero <miguel_angel_g@live.com>
2017-07-20 17:13:04 -0600

    Included time stamps

--- RealAppAnalysis/Node.js/simple.js
+++ RealAppAnalysis/Node.js/simple.js
@@ -1,3 +1,38 @@
 require("async-tree")
 
-setImmediate(() => console.log("Hello!"));
+function main(){
+	submain();
+}
+
+function submain(){
+	setImmediate(() => console.log("Hello!"));
+}
+
+function performanceTest(){
+	var iterations = 1000000;
+	var cur = 0;
+	console.log("Date now")
+	var start = Date.now();
+	while(cur < iterations){
+		Date.now();
+		cur++;
+	}
+	cur = 0;
+	var end = Date.now();
+	var dateNowTime = end - start;
+	console.log(dateNowTime)
+
+	console.log("hrTime")
+	start = Date.now();
+	while(cur < iterations){
+		cur++
+		process.hrtime();
+	}
+
+	end = Date.now();
+	var hrTime = end - start;
+	console.log(hrTime)
+
+}
+
+main();
