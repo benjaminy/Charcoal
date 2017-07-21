1b5794cc86c2a64ca5dec219b59e5113d76e248f
Miguel Angel Guerrero <miguel_angel_g@live.com>
2017-07-21 07:28:49 -0600

    Goals for today

--- RealAppAnalysis/Node.js/simple.js
+++ RealAppAnalysis/Node.js/simple.js
@@ -1,38 +1,9 @@
-require("async-tree")
+require("async-tree");
+require("superstacktrace");
 
 function main(){
-	submain();
-}
-
-function submain(){
 	setImmediate(() => console.log("Hello!"));
 }
 
-function performanceTest(){
-	var iterations = 1000000;
-	var cur = 0;
-	console.log("Date now")
-	var start = Date.now();
-	while(cur < iterations){
-		Date.now();
-		cur++;
-	}
-	cur = 0;
-	var end = Date.now();
-	var dateNowTime = end - start;
-	console.log(dateNowTime)
-
-	console.log("hrTime")
-	start = Date.now();
-	while(cur < iterations){
-		cur++
-		process.hrtime();
-	}
-
-	end = Date.now();
-	var hrTime = end - start;
-	console.log(hrTime)
-
-}
-
 main();
+throw new Error();
