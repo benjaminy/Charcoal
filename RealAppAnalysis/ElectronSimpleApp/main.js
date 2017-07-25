ab8fe09e0610c483b915b6e587b2d01a4fe1a411
clararichter <clara.l.richter@gmail.com>
2017-07-25 09:46:46 -0600

    created a simple electron for testing purposes

---
+++ RealAppAnalysis/ElectronSimpleApp/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//require('async-tree');
+const electron = require('electron')
+const {app, BrowserWindow, ipcMain} = electron
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        frame: false,
+        resizable: false,
+        height: 600,
+        width: 800
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+
+});
+
+ipcMain.on('close-main-window', function () {
+    app.quit();
+});
