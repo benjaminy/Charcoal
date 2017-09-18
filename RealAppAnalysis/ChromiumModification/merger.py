eb8cb24e08b8be916e259e117056dbdf0623838a
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2017-09-18 17:23:56 -0600

    Trace merger

---
+++ RealAppAnalysis/ChromiumModification/merger.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import json
+
+def merge( basepath ):
+
+    traces = []
+
+    for fname in os.listdir( basepath ):
+        path = os.path.join( basepath, fname )
+        trace = []
+        with open( path ) as f:
+            for line in f:
+                # print line
+                try:
+                    j = json.loads( line )
+                    # print( j )
+                    trace.append( j )
+                except:
+                    pass
+        traces.append( trace )
+
+    merged = []
+
+    while len( traces ) > 0:
+        earliest = ( 0, traces[ 0 ][ 0 ] )
+        for i in range( 1, len( traces ) ):
+            # print( len( traces[ i ] ) )
+            if traces[ i ][ 0 ][ 0 ] < earliest[ 1 ][ 0 ]:
+                earliest = ( i, traces[ i ][ 0 ] )
+        # print( earliest[ 1 ] )
+        merged.append( earliest[ 1 ] )
+        traces[ earliest[ 0 ] ].pop( 0 )
+        if len( traces[ earliest[ 0 ] ] ) < 1:
+            traces.pop( earliest[ 0 ] )
+
+    return merged
+
+def test():
+    merged = merge( "Traces" )
+    print( len( merged ) )
+    for te in merged:
+        print( te )
+
+test()
