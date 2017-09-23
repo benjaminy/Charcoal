478c3f3890a2ba359b35323b07f06d809d0d21a6
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2017-09-23 10:38:42 -0600

    Added merging of Chrome Profile files

--- RealAppAnalysis/ChromiumModification/merger.py
+++ RealAppAnalysis/ChromiumModification/merger.py
@@ -3,6 +3,14 @@
 import os
 import json
 
+def timestamp( trace_entry ):
+    try:
+        rv = trace_entry[ "ts" ]
+    except:
+        rv = trace_entry[ 0 ]
+    # print( "%s  --  %s --  %s" % ( trace_entry, rv, type( rv ) ) )
+    return rv
+
 def merge( basepath ):
 
     traces = []
@@ -11,30 +19,48 @@ def merge( basepath ):
         path = os.path.join( basepath, fname )
         trace = []
         with open( path ) as f:
-            for line in f:
-                # print line
-                try:
-                    j = json.loads( line )
-                    # print( j )
-                    trace.append( j )
-                except:
-                    pass
+            try:
+                pre_trace = json.load( f )
+                print( "whole file JSON parsing WORKED! %s %s" % ( path, type( pre_trace ) ) )
+                for line in pre_trace:
+                    try:
+                        line[ "ts" ]
+                        trace.append( line )
+                    except:
+                        print( "No ts %s" % type( line ) )
+                        pass
+                def k( trace_line ):
+                    return trace_line[ "ts" ]
+                trace.sort( key=k )
+            except:
+                print( "whole file JSON parsing DID NOT WORK! %s" % path )
+                f.seek( 0 )
+                for line in f:
+                    # print line
+                    try:
+                        j = json.loads( line )
+                        # print( j )
+                        trace.append( j )
+                    except:
+                        pass
         if len( trace ) > 0:
             traces.append( trace )
 
     merged = []
-
+    print( "%d Traces" % ( len( traces ) ) )
     while len( traces ) > 0:
         earliest = ( 0, traces[ 0 ][ 0 ] )
         for i in range( 1, len( traces ) ):
             # print( len( traces[ i ] ) )
-            if traces[ i ][ 0 ][ 0 ] < earliest[ 1 ][ 0 ]:
+            if timestamp( traces[ i ][ 0 ] ) < timestamp( earliest[ 1 ] ):
                 earliest = ( i, traces[ i ][ 0 ] )
-        # print( earliest[ 1 ] )
-        merged.append( earliest[ 1 ] )
-        traces[ earliest[ 0 ] ].pop( 0 )
-        if len( traces[ earliest[ 0 ] ] ) < 1:
-            traces.pop( earliest[ 0 ] )
+        idx = earliest[ 0 ]
+        trace_line = earliest[ 1 ]
+        # print( "%s -- %s" % ( idx, trace_line ) )
+        merged.append( trace_line )
+        traces[ idx ].pop( 0 )
+        if len( traces[ idx ] ) < 1:
+            traces.pop( idx )
 
     return merged
 
