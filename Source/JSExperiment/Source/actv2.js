234fe0d1ec024f5cfe0bd7bdb8a246ae4872cfa8
Benjamin Ylvisaker <ben.ylvisaker@coloradocollege.edu>
2018-03-14 14:21:23 -0600

    v2

--- Source/JSExperiment/Source/actv2.js
+++ Source/JSExperiment/Source/actv2.js
@@ -1,6 +1,12 @@
 /* Top Matter */
 
 import assert from "assert";
+const P = Promise;
+
+function yieldP()
+{
+    return new Promise( function( res ) { setImmediate( res ); } );
+}
 
 let ATOMICIFY_TAG = Symbol( "atomicable" );
 
@@ -20,33 +26,40 @@ global_context[ SCHEDULER_TAG ] = scheduler;
 
 var current_context = global_context;
 
+/* "prelude" is a convenience function */
+function prelude()
+{
+    let actx = current_context;
+    assert( isContext( actx ) );
+    let scheduler = actx[ SCHEDULER_TAG ];
+    assert( sched.active_actxs.has( parent[ ID_TAG ] ) );
+    let atomic_stack = scheduler.atomic_stack;
+    return [ actx, scheduler, atomic_stack ];
+}
+
+/* Reminder: f can't be an async function, because we need to reset the
+ * cur_ctx global variable after every yield. */
+/* Reminder: It would be a neat convenience if atomicified functions
+ * could be called as regular async functions.  This seems to mess with
+ * the global variable hack. */
 function atomicify( f )
 {
+    /* assert( f is generator function ) */
     if( ATOMICIFY_TAG in f )
         return f;
 
     async function wrapper( ...params )
     {
-        let actx = current_context;
-        assert( isContext( actx ) );
-        let scheduler = actx[ SCHEDULER_TAG ];
-        let atomic_stack = scheduler.atomic_stack;
-        let generator_or_promise = f( ...params );
-        /* TODO: handle atomic functions */
-        if( generator_or_promise == null )
+        let [ actx, scheduler, atomic_stack ] = prelude();
+        let call_stack = actx[ CALL_STACK_TAG ];
+        let generator = f( ...params );
+        if( !( generator
+               && ( typeof generator[Symbol.iterator] === "function" ) ) )
         {
             throw new Error( "atomicify passed (" + typeof( f )
-                             + "). Needs generator function or atomic function." );
+                             + "). Needs generator function." );
         }
-        let is_generator = typeof generator_or_promise[Symbol.iterator] === "function";
-        actx[ CALL_STACK_TAG ].push( generator_or_promise );
-
-        function exitCall()
-        {
-            assert( actx[ CALL_STACK_TAG ].length > 0 );
-            let g = actx[ CALL_STACK_TAG ].pop();
-            assert( g === generator_or_promise );
-        }
+        call_stack.push( [ f, generator ] );
 
         function blockedByAtomic()
         {
@@ -55,65 +68,53 @@ function atomicify( f )
             return !not_blocked;
         }
 
-        async function notBlocked()
-        {
-            let blocks = []; /* TODO: stats about blocking */
-            while( blockedByAtomic() )
-            {
-                /* assert( actx not in scheduler.waiting_activities ); */
-                let top = atomic_stack[ atomic_stack.length - 1 ];
-                actx[ STATE_TAG ] = WAITING;
-                actx[ WAITS_TAG ]++;
-                actx[ QUEUE_LEN_TAG ] = top.waiting.length;
-                top.waiting.add( actx );
-                blocks.push( await new Promise( function( resolve, reject ) {
-                    actx[ CONTINUE_TAG ] = resolve;
-                    actx[ ABORT_TAG ]    = reject;
-                } ) );
-            }
-        }
-
         try {
             var val = undefined; /* 1st call to .next doesn't need a val */
             var is_err = false;
-            await new Promise( function( res ) { setImmediate( res ); } );
-            if( is_generator )
+            /* implicit yield before calls: */
+            await yieldP();
+            while( true ) /* return in loop, when generator is done */
             {
-                while( true ) /* return in loop, when generator is done */
+                let blocks = []; /* TODO: stats about blocking */
+                while( blockedByAtomic() )
                 {
-                    await notBlocked();
-                    current_context = actx;
+                    /* assert( actx not in scheduler.waiting_activities ); */
+                    let top = atomic_stack[ atomic_stack.length - 1 ];
+                    actx[ STATE_TAG ] = WAITING;
+                    actx[ WAITS_TAG ]++;
+                    actx[ QUEUE_LEN_TAG ] = top.waiting.length;
+                    top.waiting.add( actx );
+                    let wait = new Promise( function( resolve, reject ) {
+                        actx[ CONTINUE_TAG ] = resolve;
+                        actx[ ABORT_TAG ]    = reject;
+                    } )
+                    blocks.push( await wait );
+                }
 
-                    if( is_err )
-                        var generated = generator_or_promise.throw( val );
-                    else
-                        var generated = generator_or_promise.next( val );
+                current_context = actx;
+                if( is_err )
+                    var generated = generator.throw( val );
+                else
+                    var generated = generator.next( val );
 
-                    if( generated.done )
-                    {
-                        exitCall();
-                        return generated.value;
-                    }
+                if( generated.done )
+                    return generated.value;
 
-                    try {
-                        val = await generated.value;
-                        is_err = false;
-                    }
-                    catch( err ) {
-                        val = err;
-                        is_err = true;
-                    }
+                try {
+                    val = await generated.value;
+                    is_err = false;
+                }
+                catch( err ) {
+                    val = err;
+                    is_err = true;
                 }
             }
-            else
-            {
-                await notBlocked();
-                return await generator_or_promise;
-            }
         }
-        catch( err ) {
-            exitCall();
-            throw err;
+        finally {
+            assert( call_stack.length > 0 );
+            let [ fa, ga ] = call_stack.pop();
+            assert( ga === generator );
+            assert( fa === f );
         }
     }
 
@@ -129,7 +130,7 @@ function makeContext( child, parent )
     child[ ABORT_TAG ]      = null;
     child[ WAITS_TAG ]      = 0;
     child[ CALL_STACK_TAG ] = [];
-    child[ SCHEDULER_TAG ]  = parent ? parent[ SCHEDULER_TAG ] : null;
+    child[ SCHEDULER_TAG ]  = parent ? parent[ SCHEDULER_TAG ] : scheduler;
     child[ ID_TAG ]         = Symbol( "activity_id" );
     child[ STATE_TAG ]      = RUNNABLE;
     child[ PARENT_TAG ]     = parent;
@@ -139,25 +140,71 @@ function activateOpts( options )
 {
 return function activate( ...params_plus_f )
 {
-    let parent = current_context;
-    assert( isContext( parent ) );
-    let sched = parent[ SCHEDULER_TAG ];
-    assert( sched.active_actxs.has( parent[ ID_TAG ] ) );
-
+    let [ parent, sched, atomic_stack ] = prelude();
     console.log( "activate" );
-    var child_resolve;
-    let child = new Promise( function( resolve, reject ) {
-        child_resolve = resolve;
-    } ).then( function() {
+    var tieKnot;
+    let knot = new Promise( function( r ) { tieKnot = r } );
+    let child = async function()
+    {
+        await knot;
         makeContext( child, parent );
         assert( isContext( child ) );
         sched.all_actxs[ child[ ID_TAG ] ] = child;
         sched.num_activities++;
         assert( Object.keys( sched.all_actxs ).length === sched.num_activities );
-        if( sched.atomic_stack.length > 0 )
+        if( atomic_stack.length > 0 )
         {
             /* XXX More cases */
-            let atomic_stack = sched.atomic_stack;
+            atomic_stack[ atomic_stack.length - 1 ].peers.add( child );
+        }
+        else
+        {
+            sched.active_actxs.add( child[ ID_TAG ] );
+            current_context = child;
+        }
+        try {
+            let params = params_plus_f.slice( 0, params_plus_f.length - 1 );
+            let fn     = atomicable( params_plus_f[ params_plus_f.length - 1 ] );
+            return await fn( ...params );
+        }
+        finally {
+            /* XXX what about atomic stuff??? */
+            child[ STATE_TAG ] = FINISHED;
+            const id = child[ ID_TAG ];
+            assert( id in sched.all_actxs );
+            assert( sched.active_actxs.has( id ) );
+            delete sched.all_actxs[ id ];
+            sched.active_actxs.delete( id );
+            sched.num_activities--;
+            assert( Object.keys( sched.all_actxs ).length === sched.num_activities );
+            console.log( msg, id, sched.num_activities );
+        }
+    } ();
+
+    tieKnot();
+    /* Wrapping the child Promise in an extra Promise to force the
+     * caller of activate to yield so that current_context is set
+     * properly. */
+    return P.resolve( c );
+}
+}
+
+function activateOpts( options )
+{
+return function activate( ...params_plus_f )
+{
+    let [ parent, sched, atomic_stack ] = prelude();
+    console.log( "activate" );
+
+    let child = R.resolve().then( function() {
+        makeContext( child, parent );
+        assert( isContext( child ) );
+        sched.all_actxs[ child[ ID_TAG ] ] = child;
+        sched.num_activities++;
+        assert( Object.keys( sched.all_actxs ).length === sched.num_activities );
+        if( atomic_stack.length > 0 )
+        {
+            /* XXX More cases */
             atomic_stack[ atomic_stack.length - 1 ].peers.add( child );
         }
         else
@@ -212,12 +259,9 @@ function atomicOpts( options )
 {
 return async function atomic( ...params_plus_fn )
 {
-    let actx = current_context;
-    assert( isContext( actx ) );
-    let scheduler = actx[ SCHEDULER_TAG ];
-    assert( scheduler.active_actxs.has( actx[ ID_TAG ] ) );
+    let [ parent, sched, atomic_stack ] = prelude();
+    console.log( "atomic" );
 
-    let atomic_stack = scheduler.atomic_stack;
     let new_top      = {};
     new_top.peers = new Set( scheduler.active_actxs );
     scheduler.active_actxs = new Set( [ actx ] );
